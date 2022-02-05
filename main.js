d4c071246d752de95aee1d6c976cfe63b8622d98
Dimon1UA <92036555+Dimon1UA@users.noreply.github.com>
2022-02-05 14:08:36 +0200

    for

---
+++ main.js
@@ -0,0 +1,34 @@
+// For №1
+
+// let y;
+// for(let x = 1; x <= 10; x++) {
+//     y = x**2
+//     console.log(y)
+// }
+
+// For №2
+
+// let n = +prompt('Введите конечное значение n')
+// let s = 0;
+// for (let k = 1; k <= n; k++) {
+//     s+=k
+// }
+// console.log(s)
+
+// For №3
+
+// let a = 10000;
+// for(let g = 0; g < 5; g++) {
+//     a+=a*0.1
+// }
+// console.log(a)
+
+// For №4
+
+// alert('Таблица умножения:')
+// for (let i = 2; i <= 10; i++) {
+//     for(let j = 1; j < 11; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`)
+//     }
+//     console.log('-------------------')
+// }
